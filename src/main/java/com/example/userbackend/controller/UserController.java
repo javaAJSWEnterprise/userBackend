fc2d8be9fcae99085d52dac0dddc53fbb4c7b2d3
NicoHerlan <nicodvkcss@gmail.com>
2023-10-16 20:45:21 -0300

    Change Port

--- src/main/java/com/example/userbackend/controller/UserController.java
+++ src/main/java/com/example/userbackend/controller/UserController.java
@@ -14,27 +14,22 @@ import java.util.Optional;
 public class UserController {
 
     private final UserService userService;
-
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
     @PostMapping
     public UserResponse createUser(@RequestBody UserRequest user) {
         return userService.createUser(user);
     }
-
     @GetMapping("/{userId}")
     public Optional<UserResponse> getUserById(@PathVariable String userId) {
         return userService.getUserById(userId);
     }
-
     @PutMapping("/{userId}")
     public UserResponse updateUser(@PathVariable String userId, @RequestBody UserUpdateRequest user) {
         return userService.updateUser(userId, user);
     }
-
     @PostMapping("/login")
     public Optional<UserResponse> login(@RequestBody Authentication authentication){
         return userService.login(authentication);
